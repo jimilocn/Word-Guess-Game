e55e5094b50f6367cd40f75edc066dfd00130ac4
Jimi Loc Nguyen <jimilocn@gmail.com>
2019-05-10 14:37:03 -0500

    added pseudocode from Phil

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-var wordOptions = [
+// var inspired by the pseudocode from Professor Phil
+var neighborhoodOptions = [
   "Streeterville",
   "Wrigleyville",
   "Andersonville",
@@ -13,3 +14,104 @@ var wordOptions = [
   "Greektown",
   "Ravenswood",
 ];
+
+var computerRandIndex = math.floor(math.random() * neighborhoodOptions.length);
+
+var computerPickWord = neighborhoodOptions[computerRandIndex];
+
+var numberofGuessCount = 9
+
+var wins = 0;
+
+var losses = 0;
+
+var wrongGuessLetter = "";
+
+var rightGuessLetter = "";
+
+var boardGame = [];
+
+for (var i = 0; i < computerPickWord; i++) {
+  boardGame[i] = "_";
+}
+
+var userInput = "";
+
+function resetGame() {
+  var neighborhoodOptions = [
+    "Streeterville",
+    "Wrigleyville",
+    "Andersonville",
+    "Edgewater",
+    "Chinatown",
+    "Englewood",
+    "Pilson",
+    "Bucktown",
+    "Bronzeville",
+    "Boystown",
+    "Bucktown",
+    "Greektown",
+    "Ravenswood",
+  ];
+
+  var computerRandIndex = math.floor(math.random() * neighborhoodOptions.length);
+
+  var computerPickWord = neighborhoodOptions[computerRandIndex];
+
+  var numberofGuessCount = 9
+
+  var wins = 0;
+
+  var losses = 0;
+
+  var wrongGuessLetter = "";
+
+  var rightGuessLetter = "";
+
+  var boardGame = [];
+
+  for (var i = 0; i < computerPickWord; i++) {
+    boardGame[i] = "_";
+  }
+
+  var userInput = "";
+}
+
+
+
+document.onkeyup = function (event) {
+
+  userInput = event.key.toLowerCase();
+
+
+  var displayBoardDiv = document.getElementbyID("guesses");
+  displayBoardDiv.textContent = boardGame.join("");
+
+
+
+  if (computerPickWord.indexOf(userInput) > -1) {
+
+    boardGame[computerPickWord.indexOf(userInput)] = userInput;
+    displayBoard.textContent = boardGame.join("guesses");
+
+    rightGuessLetter = rightGuessLetter + userInput;
+  }
+    if (computerPickWord === rightGuessLetter) {
+
+      wins++;
+      resetGame();
+    }
+    else {
+      numberofGuessCount = numberofGuessCount - 1;
+      wrongGuessLetter = wrongGuessLetter + userInput + ",";
+      var outputWrongDiv = document.getElementbyID("lettersGuessed");
+      outputWrongDiv.textContext = wrongGuessLetter;
+
+
+      if (numberofGuessCount === 0) {
+        loss++;
+        resetGame();
+      }
+
+    }
+  }
